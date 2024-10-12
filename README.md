00b3b8f89a264db9508159ee5bd10ddf6401c14b
Asi <64479526+MachNacho@users.noreply.github.com>
2024-10-12 07:50:30 +0200

    added quiz model

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 # Welcome
 This readme will server as a guide between developers and documentation of API.
 ## Guide of development
+
 ## API documentation
---
+++ api/Models/Quiz.cs
@@ -0,0 +1,10 @@
+namespace api.Models
+{
+    public class Quiz
+    {
+        public int Id { get; set; }
+        public string quizTitle { get; set; }
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+        public int TimeLimitSeconds { get; set; }
+    }
+}
