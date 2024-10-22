0edcb3fec40250e4fec37e18ff7cfa001c82e016
Asi <64479526+MachNacho@users.noreply.github.com>
2024-10-22 07:40:39 +0200

    Update ApplicationDBContext.cs

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -14,10 +14,9 @@ namespace api.Data
         public ApplicationDBContext(DbContextOptions dbContextOptions):base(dbContextOptions)
         {
         }
-<<<<<<< Updated upstream
-=======
+
         public DbSet<Quiz> quiz {get; set;}
->>>>>>> Stashed changes
+
         // creating roles within the DB
         protected override void OnModelCreating(ModelBuilder builder){
             base.OnModelCreating(builder);
